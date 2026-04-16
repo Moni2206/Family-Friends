4457c8328c9883ebe3cf68a5f4e51fab8855ed4c
Monika <moha0009@stud.kea.dk>
2026-04-16 17:01:20 +0200

    new done

--- src/components/ListerPet.jsx
+++ src/components/ListerPet.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import Favorit from "./Favorit";
 
@@ -9,8 +8,10 @@ export default function ListerPet({ id, breedGroup, origin, image }) {
 
       <Link href={`/detail/${id}`}>
         <div className="flex flex-col cursor-pointer">
-          <div className="w-full">{image && <Image src={image} alt={breedGroup} width={400} height={300} className="w-full h-40 object-cover rounded-b-lg" />}</div>
+          {/* IMAGE (safe version - ingen crash) */}
+          {image ? <img src={image} alt={breedGroup} className="w-full h-40 object-cover rounded-b-lg" /> : <div className="w-full h-40 bg-gray-200 flex items-center justify-center">Ingen billede </div>}
 
+          {/* TEXT */}
           <div className="px-3 pb-4 pt-2">
             <h2 className="font-bold text-lg">{breedGroup}</h2>
             <p className="text-gray-400 text-sm">{origin}</p>
